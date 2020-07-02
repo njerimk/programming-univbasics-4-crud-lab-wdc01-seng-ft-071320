75b6ecca456733c5a55859369854a46b70077711
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-02 16:13:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array=["peonies","petunias","sakura blossoms"]
+  array=["wow","petunias","sakura blossoms"]
   array[0]
 end
 
