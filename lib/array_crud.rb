8157be757485ccccdb47f0a5580f5ba373333cc5
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-02 16:09:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array=["queen_of_spades","king_of_hearts","joker", "ace_of_spades"]
-  p array.shift()
+  p array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
